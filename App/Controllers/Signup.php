fa1853d0473e5f5b18a3595bb20f2cfa396dab47
Darrell Ulm <darrellulm@gmail.com>
2017-06-13 16:39:47 -0400

    user validation

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -2,18 +2,44 @@
 
 namespace App\Controllers;
 
-use Core\View;
-use App\Models\User;
+use \Core\View;
+use \App\Models\User;
 
-class Signup extends \Core\Controller {
-    
-    public function newAction() {
+/**
+ * Signup controller
+ *
+ * PHP version 7.0
+ */
+class Signup extends \Core\Controller
+{
+
+    /**
+     * Show the signup page
+     *
+     * @return void
+     */
+    public function newAction()
+    {
         View::renderTemplate('Signup/new.html');
     }
-    
-    public function createAction() {
+
+    /**
+     * Sign up a new user
+     *
+     * @return void
+     */
+    public function createAction()
+    {
         $user = new User($_POST);
-        $user->save();
-        View::renderTemplate('Signup/success.html');
+
+        if ($user->save()) {
+
+           View::renderTemplate('Signup/success.html');
+
+       } else {
+
+           var_dump($user->errors);
+
+       }
     }
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use PDO;
 
 /**
- * Example user model
+ * User model
  *
  * PHP version 7.0
  */
@@ -13,45 +13,87 @@ class User extends \Core\Model
 {
 
     /**
-     * 
-     * @param type $data
+     * Error messages
+     *
+     * @var array
      */
-    public function __construct($data) {
+    public $errors = [];
+
+    /**
+     * Class constructor
+     *
+     * @param array $data  Initial property values
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
         foreach ($data as $key => $value) {
             $this->$key = $value;
-        }
+        };
     }
-    
-    
+
     /**
-     * 
-     */
-    public function save() {
-        $password_hash = \password_hash($this->password, PASSWORD_DEFAULT);
-        
-        $sql = 'INSERT INTO users (name, email, password_hash)
-                VALUES (:name, :email, :password_hash)';
-        $db = static::getDB();
-        $stmt = $db->prepare($sql);
-        
-        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
-        
-        $stmt->execute();
-    }
-    
-    
-    /**
-     * Get all the users as an associative array
+     * Save the user model with the current property values
      *
-     * @return array
+     * @return boolean  True if the user was saved, false otherwise
      */
-    /*public static function getAll()
+    public function save()
     {
-        $db = static::getDB();
-        $stmt = $db->query('SELECT id, name FROM users');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-    
+        $this->validate();
+
+        if (empty($this->errors)) {
+          
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (name, email, password_hash)
+                    VALUES (:name, :email, :password_hash)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate current property values, adding valiation error messages to the errors array property
+     *
+     * @return void
+     */
+    public function validate()
+    {
+       // Name
+       if ($this->name == '') {
+           $this->errors[] = 'Name is required';
+       }
+
+       // email address
+       if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+           $this->errors[] = 'Invalid email';
+       }
+
+       // Password
+       if ($this->password != $this->password_confirmation) {
+           $this->errors[] = 'Password must match confirmation';
+       }
+
+       if (strlen($this->password) < 6) {
+           $this->errors[] = 'Please enter at least 6 characters for the password';
+       }
+
+       if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+           $this->errors[] = 'Password needs at least one letter';
+       }
+
+       if (preg_match('/.*\d+.*/i', $this->password) == 0) {
+           $this->errors[] = 'Password needs at least one number';
+       }
+    }
 }

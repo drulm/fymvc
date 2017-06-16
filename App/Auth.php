431b98f81cee8ec05d14870ca0b04dd165bd9537
Darrell Ulm <darrellulm@gmail.com>
2017-06-16 19:46:58 -0400

    remove cookie when logout and auth from database

--- App/Auth.php
+++ App/Auth.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Models\User;
+use App\Models\RememberedLogin;
 
 /**
  * Authentication
@@ -27,8 +28,11 @@ class Auth
 
         if ($remember_me) {
 
-            $user->rememberLogin();
+            if ($user->rememberLogin()) {
 
+                setcookie('remember_me', $user->remember_token, $user->expiry_timestamp, '/');
+
+            }
         }
     }
 
@@ -39,26 +43,28 @@ class Auth
      */
     public static function logout()
     {
-      // Unset all of the session variables
-      $_SESSION = [];
+        // Unset all of the session variables
+        $_SESSION = [];
 
-      // Delete the session cookie
-      if (ini_get('session.use_cookies')) {
-          $params = session_get_cookie_params();
+        // Delete the session cookie
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
 
-          setcookie(
-              session_name(),
-              '',
-              time() - 42000,
-              $params['path'],
-              $params['domain'],
-              $params['secure'],
-              $params['httponly']
-          );
-      }
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
 
-      // Finally destroy the session
-      session_destroy();
+        // Finally destroy the session
+        session_destroy();
+
+        static::forgetLogin();        
     }
 
     /**
@@ -91,6 +97,57 @@ class Auth
         if (isset($_SESSION['user_id'])) {
 
             return User::findByID($_SESSION['user_id']);
+
+        } else {
+
+            return static::loginFromRememberCookie();
+        }
+    }
+
+    /**
+     * Login the user from a remembered login cookie
+     *
+     * @return mixed The user model if login cookie found; null otherwise
+     */
+    protected static function loginFromRememberCookie()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if ($cookie) {
+
+            $remembered_login = RememberedLogin::findByToken($cookie);
+
+            if ($remembered_login && ! $remembered_login->hasExpired()) {
+
+                $user = $remembered_login->getUser();
+
+                static::login($user, false);
+
+                return $user;
+            }
+        }
+    }
+
+    /**
+     * Forget the remembered login, if present
+     *
+     * @return void
+     */
+    protected static function forgetLogin()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if ($cookie) {
+
+            $remembered_login = RememberedLogin::findByToken($cookie);
+
+            if ($remembered_login) {
+
+                $remembered_login->delete();
+
+            }
+
+            setcookie('remember_me', '', time() - 3600);  // set to expire in the past
         }
     }
 }
--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -33,7 +33,7 @@ class Login extends \Core\Controller
     public function createAction()
     {
         $user = User::authenticate($_POST['email'], $_POST['password']);
-
+        
         $remember_me = isset($_POST['remember_me']);
 
         if ($user) {

233426086efb1d6156e88d17cddeb2afbcc5d205
Darrell Ulm <darrellulm@gmail.com>
2017-06-22 12:32:40 -0400

    Add mysql blog table creation to sql file

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS remembered_logins (
   PRIMARY KEY (`token_hash`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- 
+
 CREATE TABLE IF NOT EXISTS users (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
@@ -20,3 +20,14 @@ CREATE TABLE IF NOT EXISTS users (
   UNIQUE KEY `password_reset_hash` (`password_reset_hash`),
   UNIQUE KEY `activation_hash` (`activation_hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS blog (
+  `blog_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post` longtext NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `timestamp` varchar(45) NOT NULL,
+  `user_id` int(11) DEFAULT '0',
+  PRIMARY KEY (`blog_id`),
+  KEY `fk_user_idx` (`user_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;

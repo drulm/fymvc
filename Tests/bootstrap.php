92334d0a5d1c08e9a9a381a6835ef845f3db842b
Darrell Ulm <darrellulm@gmail.com>
2017-06-26 18:04:06 -0400

    Start of testing code

---
+++ Tests/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author grace
+ */
+// TODO: check include path
+ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../../../../home/grace/Downloads/nb_templates'.PATH_SEPARATOR.dirname(__FILE__).'/../../vendor');
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__DIR__) . '/vendor/autoload.php');
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// put your code here
+
+/*
+include_once 'Core/Controller.php';
+include_once 'Core/Model.php';
+include_once 'App/Controllers/Authenticated.php';
+
+include_once 'App/Controllers/Post.php';
+include_once 'App/Models/Blog.php';
+*/
+
